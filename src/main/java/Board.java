a3a96c418c7727182f68543e4a2770aa918c287a
Mihkel Vaher <mihkelvaher91@gmail.com>
2015-03-07 20:39:13 +0200

    Green light Board and fixed BoardTest

--- src/main/java/Board.java
+++ src/main/java/Board.java
@@ -1,31 +1,92 @@
 public class Board {
     private Cell[][] grid;
+    private int height=3; //bottom right pos: grid[height-1][width-1]
+    private int width=3;
 
     public Board(Cell[][] grid) {
         this.grid = grid;
     }
 
     /**
-     * @param size
+     * @param height
+     * @param width
      * @param p probability that Cell is alive at start
      */
-    public Board(int size, double p) {
-        grid = new Cell[size][size];
-        // TODO: initialize Cells with p alive probability.
+    public Board(int height, int width, double p) {
+        this.height=height;
+        this.width = width;
+        grid = new Cell[height][width];
+        
+        for (int h=0; h<grid.length; h++){
+            for (int w=0; w<grid[h].length; w++){
+                grid[h][w] = new Cell();
+                if (Math.random()<=p){
+                    grid[h][w].setNewState(true);
+                    grid[h][w].updateState();
+                }
+            }
+        }
     }
 
     public Cell[][] getGrid() {
         return grid;
     }
 
-    public int neighboursCountAt(int i, int j) {
-        // TODO: Implement this method
+    public int neighboursCountAt(int row, int col) {
+        int sum=0;
+        // Positions numbered as phone dial
+        if (row != 0 && col != 0){    //1
+            if(isAlive(row-1,col-1)){
+                sum++;
+            }
+        }
         
-        return -1;
+        if (row != 0){
+            if(isAlive(row-1,col)){ //2
+            sum++;
+            }
+        }
+        
+        if (row != 0 && col != width-1){//3
+            if(isAlive(row-1,col+1)){
+                sum++;
+            }
+        }
+        if (col != 0){
+            if(isAlive(row,col-1)){ //4
+            sum++;
+            }
+        }
+        //self
+        if (col != width-1){
+            if(isAlive(row,col+1)){ //6
+                sum++;
+            }
+        }
+
+        if (row != height-1 && col != 0){
+            if(isAlive(row+1,col-1)){ //7
+                sum++;
+            }
+        }
+
+        if (row != height-1){
+            if(isAlive(row+1,col)){ //8
+            sum++;
+            }
+        }
+
+        if (row != height-1 && col != width-1){
+            if(isAlive(row+1,col+1)){ //9
+                sum++;
+            }
+        }
+
+        return sum;
     }
 
-    public boolean isAlive(int i, int j) {
-        return grid[i][j].getState();
+    public boolean isAlive(int row, int col) {
+        return grid[row][col].getState();
     }
 
     public void update() {
@@ -38,13 +99,27 @@ public class Board {
      * according to GoF rules
      */
     private void prepare() {
-        
+        for (int h=0; h<grid.length; h++){
+            for (int w=0; w<grid[h].length; w++){
+                int nr = neighboursCountAt(h,w);                
+                if (nr < 2) { grid[h][w].setNewState(false);}  //underpop
+                else if (nr > 3) { grid[h][w].setNewState(false);} //overcrowd
+                else if (nr == 3) { grid[h][w].setNewState(true);} //born
+                else if (nr == 2) { grid[h][w].setNewState(grid[h][w].getState());} // stay same
+            }
+        }
     }
 
     /**
      * Updates Cell state based on newState
      */
     private void commit() {
-        
+        for (int h=0; h<grid.length; h++){
+            for (int w=0; w<grid[h].length; w++){
+                grid[h][w].updateState();
+            }
+        }
     }
+    
+    
 }
--- src/test/java/BoardTest.java
+++ src/test/java/BoardTest.java
@@ -3,10 +3,17 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class BoardTest {
-
+   
     @Test
     public void testGetNeighbours() {
-        Board b = new Board(3, 0.0);
+        //Board b = new Board(3, 3, 0.0);
+        
+        Cell[][] cells = {
+                {new Cell(true), new Cell(true), new Cell(true)}, 
+                {new Cell(true), new Cell(true), new Cell(true)}, 
+                {new Cell(true), new Cell(true), new Cell(true)}
+            };
+        Board b = new Board(cells);
 
         assertEquals(3, b.neighboursCountAt(0,0));
         assertEquals(8, b.neighboursCountAt(1,1));
@@ -63,9 +70,9 @@ public class BoardTest {
         b.update();
         
         assertEquals(true, b.isAlive(0, 0));
-        assertEquals(true, b.isAlive(2, 0));
+        assertEquals(true, b.isAlive(0, 2));
         assertEquals(true, b.isAlive(1, 0));
-        assertEquals(true, b.isAlive(2, 1));
+        assertEquals(true, b.isAlive(1, 2));
         
         assertEquals(false, b.isAlive(0, 1));
         assertEquals(false, b.isAlive(1, 1));
