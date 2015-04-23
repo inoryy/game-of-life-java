6ff0bad850275b1773e4afca6d2fb596b03b3837
Roman Marintšenko <inoryy@gmail.com>
2015-04-23 14:12:02 +0300

    add template class for gui

---
+++ src/main/java/ee/ut/oop/gof/gui/GameOfLife.java
@@ -0,0 +1,22 @@
+package ee.ut.oop.gof.gui;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class GameOfLife extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent parent = FXMLLoader.load(getClass().getResource("gui.fxml"));
+        primaryStage.setScene(new Scene(parent));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
+
