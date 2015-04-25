7045e3bf3d7d7250ea99a677a8d69d907451c32e
Mihkel Vaher <mihkelvaher91@gmail.com>
2015-04-25 23:05:47 +0300

    clear button and about added

--- src/main/java/gof/gui/Controller.java
+++ src/main/java/gof/gui/Controller.java
@@ -16,19 +16,33 @@ import java.util.Scanner;
 
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
+import javafx.event.ActionEvent;
 import javafx.event.Event;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
 import javafx.scene.Group;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Hyperlink;
 import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.control.Slider;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
+import javafx.scene.text.TextFlow;
 import javafx.stage.FileChooser;
+import javafx.stage.Modality;
+import javafx.stage.Popup;
 import javafx.stage.FileChooser.ExtensionFilter;
 import javafx.stage.Stage;
 import javafx.util.Duration;
@@ -66,6 +80,8 @@ public class Controller implements Initializable {
     private Button stopButton;
     @FXML
     private Button randomizeButton;
+    @FXML
+    private Button clearButton;
     
     private Board board;
     
@@ -78,6 +94,7 @@ public class Controller implements Initializable {
     public void initialize(URL location, ResourceBundle resources) {
     	createBoard(10, 10, 0.3);
     	//todo: get and set presets
+    	
     }
 
     @FXML
@@ -99,6 +116,7 @@ public class Controller implements Initializable {
         saveButton.setDisable(true);
         runButton.setDisable(true);
         randomizeButton.setDisable(true);
+        clearButton.setDisable(true);
         stopButton.setDisable(false);
         /////////// END OF DISABLE/ENABLE BLOCK ///////////
         loop = new Timeline(new KeyFrame(Duration.millis(300), e -> {
@@ -253,14 +271,56 @@ public class Controller implements Initializable {
         });
     }
     
+    @FXML
+    private void onClear(Event evt) {
+        createBoard(10, 10, 0);
+    }
+    
+    
+    @FXML
+    private void onAbout(Event evt) {
+        // TEXT //
+        Text text1 = new Text("Conway's Game of Life\n");
+        text1.setFont(Font.font(30));
+        Text text2 = new Text(
+                  "\nThe Game of Life, also known simply as Life, is a cellular automaton devised by the British mathematician John Horton Conway in 1970.\n"
+                + "The game is a zero-player game, meaning that its evolution is determined by its initial state, requiring no further input. One interacts with the Game of Life by creating an initial configuration and observing how it evolves or, for advanced players, by creating patterns with particular properties."
+                );
+        Text text3 = new Text("\n\nRules\n");
+        text3.setFont(Font.font(20));
+        Text text4 = new Text(
+                "\nThe universe of the Game of Life is a two-dimensional orthogonal grid of square cells, each of which is in one of two possible states, alive or dead. Every cell interacts with its eight neighbours, which are the cells that are horizontally, vertically, or diagonally adjacent. At each step in time, the following transitions occur:\n"
+                        +"\n1) Any live cell with fewer than two live neighbours dies, as if caused by under-population.\n"
+                        +"2) Any live cell with two or three live neighbours lives on to the next generation.\n"
+                        +"3) Any live cell with more than three live neighbours dies, as if by overcrowding.\n"
+                        +"4) Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.\n\nMore on Wikipedia:\n"
+                );
+        
+        Hyperlink link = new Hyperlink("http://en.wikipedia.org/wiki/Conway%27s_Game_of_Life <-------not working");
+        TextFlow tf = new TextFlow(text1,text2,text3,text4,link);
+        tf.setPadding(new Insets(10, 10, 10, 10));
+        tf.setTextAlignment(TextAlignment.JUSTIFY);
+        // END TEXT, START WINDOW //
+        final Stage dialog = new Stage();
+        dialog.initModality(Modality.APPLICATION_MODAL);
+        dialog.initOwner(new Stage());
+        VBox dialogVbox = new VBox(20);
+        dialogVbox.getChildren().add(tf);
+        Scene dialogScene = new Scene(dialogVbox, 450, 500);
+        dialog.setScene(dialogScene);
+        dialog.show();
+        // END WINDOW //
+    }
+    
 
+    
     private void createBoard(int rows, int cols, double prob) {
         //board = new Board(10, 10, 0.3);
         board = new Board(rows, cols, prob);
         // for debugging
         // console = new ConsoleDriver();
         // console.displayBoard(board);
-
+        
         display = new JavaFXDisplayDriver(10, 30, board);
 
         base.getChildren().clear();
